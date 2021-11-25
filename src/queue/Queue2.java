861b5e9fb8aebbb51709ce598f7aa5200e9a2add
AleksandrShatov <shatov.alexman@gmail.com>
2021-11-25 20:49:05 +0300

    Add queue2: without items counter. Need to fix

---
+++ src/queue/Queue2.java
@@ -0,0 +1,53 @@
+package queue;
+
+//TODO Fix methods for correct work
+public class Queue2 {
+
+    private int maxSize;
+    private long[] queArray;
+    private int front;
+    private int rear;
+
+    public Queue2(int size) {
+        maxSize = size + 1;
+        queArray = new long[maxSize];
+        front = 0;
+        rear = -1;
+    }
+
+    public void insert(long value) {
+        if(rear == maxSize - 1) {
+            rear = -1;
+        }
+        queArray[++rear] = value;
+    }
+
+    public long remove() {
+        long temp = queArray[front++];
+        if(front == maxSize) {
+            front = 0;
+        }
+        return temp;
+    }
+
+    public long peek() {
+        return queArray[front];
+    }
+
+    public boolean isEmpty() {
+        return (rear + 1 == front || front + maxSize - 1 == rear);
+    }
+
+    public boolean isFull() {
+        return (rear + 2 == front || front + maxSize - 2 == rear);
+    }
+
+    public int size() {
+        if(rear >= front) {
+            return rear - front + 1;
+        } else {
+            return (maxSize - front) + (rear + 1);
+        }
+    }
+
+}
